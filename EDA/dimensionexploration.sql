7cb087f08a28e3e5ea4e20989ce96f0fd91e4864
RadhikaR0408 <radhikaprakashnayak@gmail.com>
2026-01-10 19:37:23 +0530

    Create dimensionexploration.sql

---
+++ EDA/dimensionexploration.sql
@@ -0,0 +1,30 @@
+
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+===============================================================================
+*/
+
+
+
+SELECT
+    f.season,
+    d.full_name,
+    COUNT(*) AS races,
+    SUM(f.points) AS total_points,
+    AVG(f.position) AS avg_finish_position
+FROM goldf1.fact_race_results f
+JOIN goldf1.dim_driver d
+    ON f.driver_key = d.driver_key
+GROUP BY f.season, d.driver_key
+ORDER BY f.season, total_points DESC;
+
+SELECT
+    country,
+    COUNT(*) AS circuits_count
+FROM goldf1.dim_circuit
+GROUP BY country
+ORDER BY circuits_count DESC;
